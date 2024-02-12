c0ffbebdd9776cc59e215ca3c151d1cf072d71a8
PhilipN27 <pan97g@gmail.com>
2024-02-12 00:22:16 -0500

    Created all folders needed to start adding in textures for my first item.

--- src/main/java/net/philip/mod119/item/ModItems.java
+++ src/main/java/net/philip/mod119/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.philip.mod119.item;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
 import net.philip.mod119.ModPn;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,6 +12,8 @@ public class ModItems
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModPn.MOD_ID);
 
+    public static final RegistryObject<Item> IGNITIUM = ITEMS.register("ignitium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
     public static void register (IEventBus eventBus)
     {
         ITEMS.register(eventBus);
